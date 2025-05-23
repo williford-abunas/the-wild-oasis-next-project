a92509c78d6f0f9ec9088577ad5790bd40b05146
williford-abunas <abunaswilliford@gmail.com>
2025-05-24 02:59:56 +1200

    loading page/serverside data fetching

--- app/cabins/page.tsx
+++ app/cabins/page.tsx
@@ -1,8 +1,15 @@
-
-export default function Page() {
+import Counter from "../components/Counter"
+export default async function Page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users")
+  const data = await res.json()
+  console.log(data)
   return (
     <>
       <h1>Cabins</h1>
+      <ul>
+        {data.map((usr) => <li key={usr.id}>{usr.name}</li>)}
+      </ul>
+      <Counter users={data}/>
     </>
   )
 }

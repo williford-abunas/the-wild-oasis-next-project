1dd07c26cef40d9c8d14ddd64e742e17cc3f8292
williford-abunas <abunaswilliford@gmail.com>
2025-05-24 22:15:05 +1200

    setup pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
-import Navigation from './_components/Navigation'
-import Logo from './_components/Logo'
-
 import { Josefin_Sans } from 'next/font/google'
 import '@/app/_styles/globals.css'
+import Header from './_components/Header'
 
 const josefin = Josefin_Sans({
   subsets: ['latin'],
@@ -26,14 +24,12 @@ export default function RootLayout({ children }: ChildrenProps) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
       >
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copywright by POCODEV</footer>
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,29 @@
 import Link from 'next/link'
+import Image from 'next/image'
+import bg from '@/public/bg.png'
 
 export default function Page() {
   return (
-    <>
-      <h1>Welcome Wild Oasis!</h1>
-      <Link href="/cabins">Explore luxury cabins</Link>
-    </>
+    <div className="mt-24">
+      <Image
+        src={bg}
+        alt="Mountains and forests with two cabins"
+        className="object-cover object-top"
+        placeholder="blur"
+        fill
+      />
+
+      <div className="relative z-10 text-center">
+        <h1 className="text-8xl text-primary-50 mb-10 tracking-tight font-normal">
+          Welcome to paradise.
+        </h1>
+        <Link
+          href="/cabins"
+          className="bg-accent-500 px-8 py-6 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all"
+        >
+          Explore luxury cabins
+        </Link>
+      </div>
+    </div>
   )
 }

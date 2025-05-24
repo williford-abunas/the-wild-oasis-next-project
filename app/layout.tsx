a8e4c6031bf0423dc4ec1dcdecaf962219a664bf
williford-abunas <abunaswilliford@gmail.com>
2025-05-24 13:00:09 +1200

    organize folder structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,21 @@
-import Navigation from './components/Navigation'
-import Logo from './components/Logo'
+import Navigation from './_components/Navigation'
+import Logo from './_components/Logo'
 
 type ChildrenProps = {
   children: React.ReactNode
 }
 
 export const metadata = {
-  title: "The Wild Oasis",
+  title: 'The Wild Oasis',
 }
 
-export default function RootLayout({ children } : ChildrenProps) {
+export default function RootLayout({ children }: ChildrenProps) {
   return (
     <html lang="en">
       <body>
         <header>
-        <Logo />
-        <Navigation />
+          <Logo />
+          <Navigation />
         </header>
         <main>{children}</main>
         <footer>Copywright by POCODEV</footer>

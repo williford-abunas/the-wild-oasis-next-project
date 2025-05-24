6d78d73eafffcaa7c6c5591961ba9b024194a6e1
williford-abunas <abunaswilliford@gmail.com>
2025-05-24 16:56:22 +1200

    implement metadata and load font

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,3 +1,6 @@
+export const metadata = {
+  title: 'Account',
+}
 
 export default function Page() {
   return (
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,33 @@
 import Navigation from './_components/Navigation'
 import Logo from './_components/Logo'
 
+import { Josefin_Sans } from 'next/font/google'
+import '@/app/_styles/globals.css'
+
+const josefin = Josefin_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+})
+
 type ChildrenProps = {
   children: React.ReactNode
 }
 
 export const metadata = {
-  title: 'The Wild Oasis',
+  title: {
+    template: '%s / The Wild Oasis',
+    default: 'Welcome / The Wild Oasis',
+  },
+  description:
+    'Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forest.',
 }
 
 export default function RootLayout({ children }: ChildrenProps) {
   return (
     <html lang="en">
-      <body>
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+      >
         <header>
           <Logo />
           <Navigation />

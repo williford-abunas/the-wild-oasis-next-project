098b5257e6631baeb68953a6402fd9d94538d115
williford-abunas <abunaswilliford@gmail.com>
2025-05-30 21:12:50 +1200

    implement ISR with revalidate fn

--- app/cabins/page.tsx
+++ app/cabins/page.tsx
@@ -2,6 +2,12 @@ import { Suspense } from "react";
 import CabinList from "@/app/_components/CabinList";
 import Spinner from "@/app/_components/Spinner";
 
+export const revalidate = 3600;
+
+export const metadata = {
+  title: "Cabins",
+};
+
 export default function Page() {
   return (
     <div>
